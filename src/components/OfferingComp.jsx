f504ee33d41baf4bfa29e2958c9b74e0be40cf5a
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-02-12 01:10:37 +0530

    content-update-for Offering

--- src/components/OfferingComp.jsx
+++ src/components/OfferingComp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function OfferingComp({img ,title, description}) {
   return (
     <div>
-          <div className='h-80 w-80 drop-shadow-white bg-white border border-blue-1 '>
+          <div className='h-80 w-80 drop-shadow-white rounded-md bg-white border border-blue-1 '>
               <img src={img} alt="offerings-bg" className='w-16 h-16 mx-auto mt-10' />
               <p className='text-black text-lg font-bold mt-7 text-center '>{title}</p>
               <p className='text-black text-sm font-light leading-relaxed mt-7 text-center px-12'>{description}</p>
