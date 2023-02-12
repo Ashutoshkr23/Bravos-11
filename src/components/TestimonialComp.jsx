9cff6472c60394e09fdcbed290218450506579ee
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-02-12 13:55:46 +0530

    images and content added for testimonials

--- src/components/TestimonialComp.jsx
+++ src/components/TestimonialComp.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function TestimonialComp() {
+function TestimonialComp({ feedback, img, name, designation }) {
   return (
     <div className='relative h-[437px] w-[364px] mt-16 bg-white rounded-md'>
-          <img src="./assets/images/team/Sid.png" alt="" className='absolute inset-x-0 mx-auto bottom-[115px] z-10' />
+          <img src={img} alt="" className='absolute inset-x-0 mx-auto bottom-[115px] z-10' />
         <div className='w-[308px] pt-7 px-7'>
-              <p className='text-sm font-bold w-[308px] leading-[1.4rem]'>The Bravas Digital team is highly responsive and is very quick on the uptake of our ever-evolving business strategies. They are an excellent partner for your digital communication requirements.</p>
+              <p className='text-sm font-bold w-[308px] leading-[1.4rem]'>{feedback}</p>
         </div>
         <div className='bg-dark rounded-md h-[186px] absolute bottom-0 inset-x-0'>
-            <h1 className='mt-24 text-white text-center text-lg font-bold'>ANIL NAIR</h1>
-            <p className='text-white text-center text-sm font-light'>Co-Founder, Goodwind Mototours</p>
+            <h1 className='mt-24 text-white text-center text-lg font-bold'>{name}</h1>
+            <p className='text-white text-center text-sm font-light px-8'>{designation}</p>
         </div>
       
     </div>
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -10,8 +10,39 @@ function Testimonials() {
                   <h1 className='text-black font-bold text-5xl mt-5'>CLIENT SAYS</h1>
               </div>
               <div className='px-32'>
-                  <TestimonialComp />
+                  <TestimonialComp 
+                  img={"assets/images/testimonial/Anil.png"}
+                  feedback={"The Bravas Digital team is highly responsive and is very quick on the uptake of our ever-evolving business strategies. They are an excellent partner for your digital communication requirements."}
+                  name={"Siddharth Nair"}
+                  designation={"Co-Founder, Goodwind Mototours"}
+            />
               </div>
+        <TestimonialComp 
+          img={"assets/images/testimonial/Sachi.png"}
+          feedback={"Every member of Bravas Digital treats my brand as their own brainchild. They understand our objectives and make sure all tasks are aligned accordingly. Highly recommend them for growing brands."}
+          name={"SUCHI SANDHU"}
+          designation={"Managing Director, Sweet Stuff Private Limited "} />
+        <TestimonialComp 
+          img={"assets/images/testimonial/Xerrxes.png"}
+          feedback={"We have been associated with Bravas Digital for the past 2 years. With an eye on ingenuity, they have consistently provided good creatives through this period."}
+          name={"XERRXEX MASTER"}
+          designation={"President, Association of Multimodal Transport Operators of India [AMTOI]"}
+        />
+        <TestimonialComp
+          img={"assets/images/testimonial/Vineet.png"}
+          feedback={"We have been very impressed with their creativity, responsiveness and capabilities in the remodelling of our company logo, creatives for our website and social media. "}
+          name={"Vineet Singhal,"}
+          designation={" Chief Operating Officer, SGN Nanopharma Inc."} />
+        <TestimonialComp 
+          img={"assets/images/testimonial/Sachit.png"}
+          feedback={"The Bravas team is dynamic and is not only supportive but also understands the pulse of our business & industry very well. The followers on our page have increased to over 500+ in less than 5 months."}
+          name={"Sachit Dedhia"}
+          designation={"Managing Director, Velocity Global Logistics Pvt. Ltd (India-China-Spain)"} />
+        <TestimonialComp 
+          img={"assets/images/testimonial/Rikshit.png"}
+          feedback={"As a start-up, it's great to have someone like the Bravas Digital Team to rely on our marketing efforts. They have a wonderful team of experts taking care of all the different aspects of marketing."}
+          name={"Rikshit Kotian,"}
+          designation={"Director - Business Development, Verraton Health Private Limited"} />
         </div>
     </div>
   )
