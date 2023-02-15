ed92489696d2ccd4987dcc395d7c7766e0410ce0
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-02-16 00:46:36 +0530

    Blur-effect

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -4,10 +4,10 @@ function Customer({ open2, setOpen2 }) {
     const logos = ['/assets/images/customer/img1.png', '/assets/images/customer/img2.png', '/assets/images/customer/img3.png', '/assets/images/customer/img4.png', '/assets/images/customer/img5.png', '/assets/images/customer/img6.png', '/assets/images/customer/img7.png', '/assets/images/customer/img8.png', '/assets/images/customer/img9.png', '/assets/images/customer/img10.png', '/assets/images/customer/img11.png', '/assets/images/customer/img12.png', '/assets/images/customer/img13.png', '/assets/images/customer/img14.png', '/assets/images/customer/img15.png', '/assets/images/customer/img16.png', '/assets/images/customer/img17.png', '/assets/images/customer/img18.png', '/assets/images/customer/img19.png', '/assets/images/customer/img20.png',];
 
     return (
-        <div className='fixed top-0 left-0  w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50' onClick={() => {
+        <div className='fixed top-0 left-0  w-full h-full flex justify-center items-center bg-black bg-opacity-50 backdrop-blur-md z-20' onClick={() => {
             setOpen2(false);
         }}>
-            <div className='w-2/5 bg-white h-full '>
+            <div className='w-2/5 bg-white h-full z-30 '>
                 <div className="grid grid-cols-4 " >
                     {logos.map((logo, index) => (
                         <div key={index} className="border px-2 py-2 border-blue-500">
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,22 +1,46 @@
 
 import React from 'react'
+import { motion } from 'framer-motion';
 
 
 function Demo() {
+
+    const icon = {
+        hidden: {
+            opacity: 0,
+            pathLength: 0,
+            fill: "rgba(255, 255, 255, 0)"
+        },
+        visible: {
+            opacity: 1,
+            pathLength: 1,
+            fill: "rgba(255, 255, 255, 1)"
+        }
+    };
     const logos = ['/assets/images/customer/img1.png', '/assets/images/customer/img2.png', '/assets/images/customer/img3.png', '/assets/images/customer/img4.png', '/assets/images/customer/img5.png', '/assets/images/customer/img6.png', '/assets/images/customer/img7.png', '/assets/images/customer/img8.png', '/assets/images/customer/img9.png', '/assets/images/customer/img10.png', '/assets/images/customer/img11.png', '/assets/images/customer/img12.png', '/assets/images/customer/img13.png', '/assets/images/customer/img14.png', '/assets/images/customer/img15.png', '/assets/images/customer/img16.png', '/assets/images/customer/img17.png', '/assets/images/customer/img18.png', '/assets/images/customer/img19.png', '/assets/images/customer/img20.png',];
 
     return (
-        <div className=' w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50' >
-            <div className='w-4/5 bg-white h-full '>
-                <div className="grid grid-cols-3 " >
-                    {logos.map((logo, index) => (
-                        <div key={index} className="border px-2 py-2 border-blue-500">
-                            <img src={logo} alt={`Logo ${index}`} className="w-full object-contain h-full" />
-                        </div>
-                    ))}
-                </div>
+           <div>
+            <div className="container">
+                <motion.svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 252 257"
+                    className="item"
+                >
+                    <motion.path
+                        d="M96.428 100.5C77.0946 99.3333 36.228 96.2 27.428 93C16.428 89 -38.572 54.5 64.928 41C90.452 38 143.1 31.6 149.5 30C157.5 28 197.5 12 118.5 2.5"
+                        variants={icon}
+                        initial="hidden"
+                        animate="visible"
+                        transition={{
+                            default: { duration: 2, ease: "easeInOut" },
+                            fill: { duration: 2, ease: [1, 0, 0.8, 1] }
+                        }}
+                    />
+                </motion.svg>
             </div>
-        </div>
+           </div>
+
     )
       }
 
