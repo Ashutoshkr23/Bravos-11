7b0edc49f58583ca15a9cb566159e0532dd29edd
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-02-17 03:10:51 +0530

    mail-and linkedin done

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -21,20 +21,25 @@ function Team() {
                                           img={"./assets/images/team/Sid.png"}
                                           designation={"CEO"}
                                           name={"SIDDHART NAIR"}
-                                          tagline={"A bearded corporate fixer"} />
+                                          tagline={"A bearded corporate fixer"}
+                                          linkedin={"https://www.linkedin.com/in/siddharth-nair-b30a3413b"}
+                                          mail={"siddharth@bravasdigital.com"} />
                                   </div>
                                   <div >
                                       <TeamComp
                                           img={"./assets/images/team/Vaishnav.png"}
                                           designation={"COO"}
                                           name={"VAISHNAV"}
-                                          tagline={"Work Invest Explore in that order"} />
+                                          tagline={"Work Invest Explore in that order"}
+                                          linkedin={"https://www.linkedin.com/in/vaishnav-manav/"}
+                                          mail={"vaishnav@bravasdigital.com"} />
                                   </div>
                                   <div>
                                       <TeamComp img={"./assets/images/team/Sandeep.png"}
                                           designation={"Advisor"}
                                           name={"SANDEEP TELANG"}
-                                          tagline={"Working up my way up Maslow’s pyramid"} />
+                                          tagline={"Working up my way up Maslow’s pyramid"}
+                                          linkedin={""} />
                                   </div>
                               </div>
                               <div className='flex mx-auto gap-10 justify-center mt-10'>
@@ -42,19 +47,22 @@ function Team() {
                                       <TeamComp img={"./assets/images/team/Krishna.png"}
                                           designation={"Digital Marketing Manager"}
                                           name={"KRISHNA RAO"}
-                                          tagline={"I believe in ideating new campaigs even in my sleep"} />
+                                          tagline={"I believe in ideating new campaigs even in my sleep"}
+                                          linkedin={"https://www.linkedin.com/in/krishna-rao-85ab6a15a"} />
                                   </div>
                                   <div >
                                       <TeamComp img={"./assets/images/team/Gorashi.png"}
                                           designation={"Digital Marketing Manager"}
                                           name={"GORANSHI JAIN"}
-                                          tagline={"Create Curate Campaign"} />
+                                          tagline={"Create Curate Campaign"}
+                                          linkedin={"https://www.linkedin.com/in/goranshi-jain-857b6715a"} />
                                   </div>
                                   <div>
                                       <TeamComp img={"./assets/images/team/Omi.png"}
                                           designation={"Senior Graphic Designer"}
                                           name={"OMKESH HODAWADEKAR"}
-                                          tagline={"Design is so simple, that's why it's so complicated"} />
+                                          tagline={"Design is so simple, that's why it's so complicated"}
+                                          linkedin={"https://www.linkedin.com/in/omi-hodawadekar-31765ba0/"} />
                                   </div>
                               </div>
                               <div className='flex mx-auto gap-10 justify-center mt-10'>
@@ -62,19 +70,22 @@ function Team() {
                                       <TeamComp img={"./assets/images/team/Aniket.png"}
                                           designation={"junior Graphic Designer"}
                                           name={"ANIKET PATIL"}
-                                          tagline={"I love everything abot Design except, it changes"} />
+                                          tagline={"I love everything abot Design except, it changes"} 
+                                          linkedin={"https://www.linkedin.com/in/aniket-patil-b35852143"}/>
                                   </div>
                                   <div >
                                       <TeamComp img={"./assets/images/team/Sourabh.png"}
                                           designation={"SEO Lead"}
                                           name={"SOURABH RAUL"}
-                                          tagline={"Empowering online visibility"} />
+                                          tagline={"Empowering online visibility"}
+                                          linkedin={""} />
                                   </div>
                                   <div>
                                       <TeamComp img={"./assets/images/team/Mahek1.png"}
                                           designation={"Digital Marketing Excutive"}
                                           name={"MAHEK SACHDEVA"}
-                                          tagline={"The 3 C s of Success : Commitment, Consistency and Confidence"} />
+                                          tagline={"The 3 C s of Success : Commitment, Consistency and Confidence"} 
+                                          linkedin={"https://www.linkedin.com/in/mahek-sachdeva-3b93a0235"}/>
                                   </div>
                               </div>
                               <div className='flex mx-auto gap-10 justify-center mt-10'>
@@ -82,7 +93,8 @@ function Team() {
                                       <TeamComp img={"./assets/images/team/Rishita1.png"}
                                           designation={"Digital Marketing Excutive"}
                                           name={"RISHITA HAREL"}
-                                          tagline={"Self-belief and Hard work will always earn you sucess"} />
+                                          tagline={"Self-belief and Hard work will always earn you sucess"} 
+                                          linkedin={"https://www.linkedin.com/in/rishita-harel-77b672207"}/>
                                   </div>
                               </div>
                           </div>
@@ -110,62 +122,74 @@ function Team() {
                                   img={"./assets/images/team/Sid.png"}
                                   designation={"CEO"}
                                   name={"SIDDHART NAIR"}
-                                  tagline={"A bearded corporate fixer"} />
+                                  tagline={"A bearded corporate fixer"} 
+                                  linkedin={"https://www.linkedin.com/in/siddharth-nair-b30a3413b"}
+                                  mail={"siddharth@bravasdigital.com"}/>
                           </div>
                           <div >
                               <TeamComp
                                   img={"./assets/images/team/Vaishnav.png"}
                                   designation={"COO"}
                                   name={"VAISHNAV"}
-                                  tagline={"Work Invest Explore in that order"} />
+                                  tagline={"Work Invest Explore in that order"}
+                                  linkedin={"https://www.linkedin.com/in/vaishnav-manav/"}
+                                  mail={""} />
                           </div>
                           <div>
                               <TeamComp img={"./assets/images/team/Sandeep.png"}
                                   designation={"Advisor"}
                                   name={"SANDEEP TELANG"}
-                                  tagline={"Working up my way up Maslow’s pyramid"} />
+                                  tagline={"Working up my way up Maslow’s pyramid"}
+                                  linkedin={""} />
                           </div>
                           <div>
                               <TeamComp img={"./assets/images/team/Krishna.png"}
                                   designation={"Digital Marketing Manager"}
                                   name={"KRISHNA RAO"}
-                                  tagline={"I believe in ideating new campaigs even in my sleep"} />
+                                  tagline={"I believe in ideating new campaigs even in my sleep"} 
+                                  linkedin={"https://www.linkedin.com/in/krishna-rao-85ab6a15a"}/>
                           </div>
                           <div >
                               <TeamComp img={"./assets/images/team/Gorashi.png"}
                                   designation={"Digital Marketing Manager"}
                                   name={"GORANSHI JAIN"}
-                                  tagline={"Create Curate Campaign"} />
+                                  tagline={"Create Curate Campaign"}
+                                  linkedin={"https://www.linkedin.com/in/goranshi-jain-857b6715a"} />
                           </div>
                           <div>
                               <TeamComp img={"./assets/images/team/Omi.png"}
                                   designation={"Senior Graphic Designer"}
                                   name={"OMKESH HODAWADEKAR"}
-                                  tagline={"Design is so simple, that's why it's so complicated"} />
+                                  tagline={"Design is so simple, that's why it's so complicated"}
+                                  linkedin={"https://www.linkedin.com/in/omi-hodawadekar-31765ba0/"} />
                           </div>
                           <div>
                               <TeamComp img={"./assets/images/team/Aniket.png"}
                                   designation={"junior Graphic Designer"}
                                   name={"ANIKET PATIL"}
-                                  tagline={"I love everything abot Design except, it changes"} />
+                                  tagline={"I love everything abot Design except, it changes"} 
+                                  linkedin={"https://www.linkedin.com/in/aniket-patil-b35852143"}/>
                           </div>
                           <div >
                               <TeamComp img={"./assets/images/team/Sourabh.png"}
                                   designation={"SEO Lead"}
                                   name={"SOURABH RAUL"}
-                                  tagline={"Empowering online visibility"} />
+                                  tagline={"Empowering online visibility"}
+                                  linkedin={""} />
                           </div>
                           <div>
                               <TeamComp img={"./assets/images/team/Mahek1.png"}
                                   designation={"Digital Marketing Excutive"}
                                   name={"MAHEK SACHDEVA"}
-                                  tagline={"The 3 C s of Success : Commitment, Consistency and Confidence"} />
+                                  tagline={"The 3 C s of Success : Commitment, Consistency and Confidence"}
+                                  linkedin={"https://www.linkedin.com/in/mahek-sachdeva-3b93a0235"} />
                           </div>
                           <div>
                               <TeamComp img={"./assets/images/team/Rishita1.png"}
                                   designation={"Digital Marketing Excutive"}
                                   name={"RISHITA HAREL"}
-                                  tagline={"Self-belief and Hard work will always earn you sucess"} />
+                                  tagline={"Self-belief and Hard work will always earn you sucess"}
+                                  linkedin={"https://www.linkedin.com/in/rishita-harel-77b672207"} />
                           </div>
                       </div>
                   </div>
