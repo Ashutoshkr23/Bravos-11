71d68c74a78c96acff5089a909004e39def55791
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-02-18 02:58:51 +0530

    Navbar-opacity-change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
       <div>
           {/*Laptop */}
           <div className='hidden bg-dark lg:block'>
-              <div className='navbar z-50 fixed inset-x-0 bg-dark opacity-90'>
+              <div className='navbar z-50 fixed inset-x-0 bg-dark opacity-95'>
                   <div className='mx-32 my-auto h-20 flex content-center justify-between' >
                       <img src="/assets/images/navbar/Logo.png" alt="Bravos Logo" className="h-12 w-28 my-auto" />
                       <div className='flex content-center'>
