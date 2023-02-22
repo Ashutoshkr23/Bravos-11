e446c932c27f517ae363c85b9753f59ff9922adf
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-02-23 00:14:57 +0530

    Drop Shadow Added

--- src/components/TeamComp.jsx
+++ src/components/TeamComp.jsx
@@ -10,7 +10,7 @@ function TeamComp({img , designation , name , tagline , linkedin , mail }) {
           onMouseLeave={() => setShowContent(false)}>
           
           <img src={img} alt="sid" className='absolute z-40 inset-x-0 mx-auto h-[113px] lg:h-[142px] w-[113px] lg:w-[142px]' />
-          <div className="w-full relative h-[180px] lg:h-[215px] bottom-0 mt-[76px] lg:mt-[96px] pt-11 lg:pt-[57px] rounded-lg bg-white-1 border-black">
+          <div className="w-full relative h-[180px] lg:h-[215px] bottom-0 mt-[76px] lg:mt-[96px] pt-11 lg:pt-[57px] rounded-lg bg-white-1 drop-shadow-white border-black">
             { showContent && (
               <div className=" absolute top-0 left-0 p-4">
                 <div className='flex gap-2 align-bottom'>
@@ -25,7 +25,7 @@ function TeamComp({img , designation , name , tagline , linkedin , mail }) {
                 </div>
               </div>
             )}
-            <div className='w-60 lg:w-[276px] h-16 lg:h-20 bg-white rounded-md py-2 mx-auto lg:py-4'>
+            <div className='w-60 lg:w-[276px] h-16 lg:h-20 bg-white drop-shadow-white rounded-md py-2 mx-auto lg:py-4'>
               <p className='text-base text-center  '>{designation}</p>
               <h1 className='font-bold text-base text-center'>{name}</h1>
             </div>
@@ -39,7 +39,7 @@ function TeamComp({img , designation , name , tagline , linkedin , mail }) {
       <div className='md:hidden'>
         <div className='bg-transparent h-40 w-[154px] hover:scale-[1.15] ease-out duration-200 border border-transparent rounded-lg relative' >
           <img src={img} alt="sid" className='absolute z-30 inset-x-0 h-16 w-16 mx-auto' />
-          <div className="w-full relative h-[114px] bottom-0 mt-11 pt-7 rounded-lg bg-white-1 border-black">
+          <div className="w-full relative h-[114px] bottom-0 mt-11 pt-7 rounded-lg bg-white-1 drop-shadow-white border-black">
             
               <div className='absolute top-1'>
                 <div className='flex justify-between mx-2 align-bottom'>
@@ -56,7 +56,7 @@ function TeamComp({img , designation , name , tagline , linkedin , mail }) {
 
               </div>
 
-            <div className='w-[138px] mx-auto  h-10 bg-white rounded-md py-2'>
+            <div className='w-[138px] mx-auto  h-10 bg-white drop-shadow-white rounded-md py-2'>
               <p className='text-[9px] text-center  '>{designation}</p>
               <h1 className='font-bold text-[10px] text-center'>{name}</h1>
             </div>
