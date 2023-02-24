b6f1cd6ffaddb7d1b43b7b49935f4b418569ca03
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-02-24 12:52:32 +0530

    opacity decreased

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,7 +8,7 @@ function Testimonials() {
       <div >
         <div className='py-6 md:pt-10 lg:pt-16'>
           <div className='pl-5 md:pl-10 lg:pl-36 relative'>
-            <p className='text-black font-light text-[10px]  md:text-xs lg:text-base  tracking-widest'>TESTIMONIALS</p>
+            <p className='text-black font-light text-[10px]  md:text-xs opacity-60 lg:text-base  tracking-widest'>TESTIMONIALS</p>
             <h1 className='text-black font-bold  text-3xl lg:text-5xl mt-5'>CLIENT SAYS</h1>
           </div>
           <div className='px:4 lg:px-12 mb-4'>
