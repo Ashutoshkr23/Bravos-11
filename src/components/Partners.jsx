f408431cde065788f3c94e0a7d354d8471826fdb
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-02-24 23:23:50 +0530

    button color channge

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -16,6 +16,12 @@ function Partners() {
     backgroundPosition: 'center'
   };
 
+  const divStyle2 = {
+    backgroundImage: 'url(./assets/images/partner/Work-bg.png)',
+    backgroundSize: 'auto',
+    backgroundPosition: 'bottom'
+  };
+
   
   return (
     <div className="bg-dark" id='work' style={divStyle}>
@@ -87,22 +93,21 @@ function Partners() {
       {/* Mobiles*/}
 
       {/*Mobiles*/}
-      <div className='lg:hidden bg-dark px-5 sm:px-12 '>
+      <div className='lg:hidden bg-dark px-auto bg-no-repeat ' style={divStyle2}>
         <div className='flex justify-between '>
-          <div> <img src="/assets/images/partner/Hanging-Bulb.png" alt="" className='invisible' /></div>
           <div>
             <div className='py-16'>
-            <p className='text-white  font-bold text-xs sm:text-sm tracking-widest opacity-60'>WORK</p>
-            <h1 className='text-white font-light text-2xl sm:text-3xl mt-2 w-56'>PARTNERS IN PROGRESS</h1>
+              <p className='text-white  font-bold text-xs sm:text-sm tracking-widest opacity-60'>WORK</p>
+              <h1 className='text-white font-light text-2xl sm:text-3xl mt-2 w-56'>PARTNERS IN PROGRESS</h1>
             <div className='mt-20  w-3/5 bg-white rounded-md py-10 mx-auto '>
               <img src="/assets/images/partner/CC.png" alt="CC" className='mx-auto  w-2/5' />
               <h3 className='mt-4 text-center font-bold text-xs sm:text-xl px-8'>CUSTOMER CONNECTIONS</h3>
             </div>
             <div className='w-full h-full flex justify-center items-center bg-transparent bg-opacity-50 z-40'>
-              <div className='w-4/5 bg-white h-full mt-8 '>
+              <div className='w-11/12 sm:w-3/4 mx-auto bg-white h-full mt-8 '>
                 <div className="grid grid-cols-3 " >
                   {logos.map((logo, index) => (
-                    <div key={index} className="border px-2 py-2 border-blue-500">
+                    <div key={index} className="border px-2 py-2 border-green-2">
                       <img src={logo} alt={`Logo ${index}`} className="w-full object-contain h-full" />
                     </div>
                   ))}
@@ -114,10 +119,10 @@ function Partners() {
               <h3 className='mt-4 text-center font-bold text-xs sm:text-xl px-8'>BUSINESS AND BEYOND</h3>
             </div>
             <div className='w-full h-full mt-8 flex justify-center items-center bg-transparent bg-opacity-50 z-40'>
-              <div className='w-4/5 bg-white h-full '>
+                <div className='w-11/12 sm:w-3/4 mx-auto bg-white h-full '>
                 <div className="grid grid-cols-3 " >
                   {logo.map((logo, index) => (
-                    <div key={index} className="border px-2 py-2 border-blue-500">
+                    <div key={index} className="border px-2 py-2 border-green-2">
                       <img src={logo} alt={`Logo ${index}`} className="w-full object-contain h-full" />
                     </div>
                   ))}
@@ -125,7 +130,6 @@ function Partners() {
               </div>
             </div>
           </div></div>
-          <div> <img src="/assets/images/partner/Hanging-Bulb.png" alt="" className='invisible' /></div>
         </div>
       </div>
     </div>
