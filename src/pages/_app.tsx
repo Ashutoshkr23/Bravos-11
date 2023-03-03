4818d3a928c91ee9ce24e7b90739756c73b133cc
alphamintlabs <rohit@alphamintlabs.com>
2023-03-03 17:42:37 +0530

    app.tsx and lib/fpixel.js updated to include fb pixel script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,59 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useEffect } from 'react';
-import { useRouter } from 'next/router'
-import TagManager from 'react-gtm-module';
-import ReactPixel from 'react-facebook-pixel';
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import TagManager from "react-gtm-module";
+// import ReactPixel from 'react-facebook-pixel';
+import Script from "next/script";
+import * as fbq from "../lib/fpixel";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  useEffect(() => {
-    import('react-facebook-pixel')
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init('6727428820606216') // facebookPixelId
-        ReactPixel.pageView()
+  const router = useRouter();
 
-        router.events.on('routeChangeComplete', () => {
-          ReactPixel.pageView()
-        })
-      })
-  }, [router.events])
+  {
+    /* Global Site Code Pixel - Facebook Pixel */
+  }
+
   useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-KWL9DRL' });
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview();
+
+    const handleRouteChange = () => {
+      fbq.pageview();
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-KWL9DRL" });
   }, []);
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+       !function(f,b,e,v,n,t,s)
+       {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+       n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+       if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+       n.queue=[];t=b.createElement(e);t.async=!0;
+       t.src=v;s=b.getElementsByTagName(e)[0];
+       s.parentNode.insertBefore(t,s)}(window, document,'script',
+       'https://connect.facebook.net/en_US/fbevents.js');
+       fbq('init', ${fbq.FB_PIXEL_ID});
+     `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
