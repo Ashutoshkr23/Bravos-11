f6a768e34846d0b986bb4af9c4700ca6b3d83c19
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-03-03 13:37:18 +0530

    changes reverse

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,7 @@ function Main() {
                 <h1 className="text-[28px] font-gotham leading-loose sm:text-4xl lg:text-6xl text-green font-bold text-center font-Gotham mb-16" style={{ backgroundImage: `url('/assets/images/main/hero-text-bg1.png')`, backgroundClip: 'text', WebkitBackgroundClip: 'text', lineHeight: '1.5' }}>TACKLING MARKETING ISSUES LIKE IT'S OUR EVERYDAY BUSINESS.</h1>
 
                 <h2 className='text-2xl sm:text-3xl lg:text-5xl text-white font-bold text-center mt-10 mb-12 font-Gotham'>HONESTLY, IT IS.</h2>
-                <p className='anim-typewriter text-xs sm:text-lg h-8 py-1 lg:text-3xl font-light text-center justify-center font-gotham lg:mt-16 text-white lg:w-[50%]'>Research Driven Approach. Quality-Based Results.</p>
+                <p className='anim-typewriter text-xs sm:text-lg  lg:text-3xl font-light text-center justify-center font-gotham lg:mt-16 text-white lg:w-[50%]'>Research Driven Approach. Quality-Based Results.</p>
             </div>
         </div>
     )
