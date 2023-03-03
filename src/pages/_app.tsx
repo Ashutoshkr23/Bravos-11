aed74babc13e497c5a421791835f66a08e6ba5df
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-03-03 17:22:58 +0530

    Added facebook pixel code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react';
+import { useRouter } from 'next/router'
 import TagManager from 'react-gtm-module';
+import ReactPixel from 'react-facebook-pixel';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('6727428820606216') // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
   useEffect(() => {
     TagManager.initialize({ gtmId: 'GTM-KWL9DRL' });
   }, []);
