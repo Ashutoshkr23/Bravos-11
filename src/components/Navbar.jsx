9c95e6a59edee8949875243e23da753a8ec4e7aa
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-03-15 13:21:15 +0530

    Footer img fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -137,15 +137,19 @@ function Navbar() {
                                           </div>
                                           <div className='mt-auto'>
                                               <div className="flex flex-row flex-wrap justify-center mx-auto my-4 min-w-0 items-center" style={{ height: '40px', borderRadius: '19px', margin: 'auto' }}>
-                                                  <a href="#" target="blank" className="w-11 h-10">
-                                                      <img className="w-6 h-6 hover:scale-125 inline-block" src={"assets/Images/footer/fb.svg"} alt="facebook icon" />
-                                                  </a>
-                                                  <a href="#" target="blank" className="w-11 h-10">
-                                                      <img className="w-6 h-6 hover:scale-125 inline-block" src="assets/Images/footer/linkedin.ico" alt="linkedin icon" />
-                                                  </a>
-                                                  <a href="#" target="blank" className="w-11 h-10">
-                                                      <img className="w-6 h-6 hover:scale-125 inline-block" src={"assets/Images/footer/instagram-light.ico"} alt="instagram icon" />
-                                                  </a>
+                                                  <div className="flex flex-row flex-wrap justify-center mx-auto my-4 min-w-0 items-center" style={{ height: '40px', width: '528px', borderRadius: '19px', margin: 'auto' }}>
+                                                      <a href="https://www.facebook.com/bravasdigital" target="blank" className="w-11 h-10">
+                                                          <img className="w-6 h-6 hover:scale-125 inline-block" src={"assets/images/footer/fb.svg"} alt="facebook icon" />
+                                                      </a>
+                                                      <a href="https://www.linkedin.com/company/bravasdigital" target="blank" className="w-11 h-10">
+                                                          <img className="w-6 h-6 hover:scale-125 inline-block" src="assets/images/footer/linkedin.ico" alt="linkedin icon" />
+                                                      </a>
+                                                      <a href="https://www.instagram.com/bravasdigital/" target="blank" className="w-11 h-10">
+                                                          <img className="w-6 h-6 hover:scale-125 inline-block" src={"assets/images/footer/instagram-light.ico"} alt="instagram icon" />
+                                                      </a>
+
+
+                                                  </div>
                                               </div>
                                               <div className="text-center pb-6 text-white text-base opacity-80 ">© {date} BY BRAVAS DIGITAL</div>
                                           </div>
