08f2d2724a6917e711416cb3bc11eaeefb7e9724
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-03-27 19:05:36 +0530

    privacy policy responsive

--- src/pages/privacypolicy.jsx
+++ src/pages/privacypolicy.jsx
@@ -5,33 +5,33 @@ function privacypolicy() {
   return (
     <div>
         <Navbar />
-        <div className='mx-28 py-40 bg-white text-black'>
-              <h1 className=' text-5xl font-bold'>PRIVACY POLICY</h1>
-              <h3 className='text-base mt-16'>INTRODUCTION:</h3>
-              <p className='text-sm'>Bravas Digital is committed to respecting and protecting the privacy of its users. This Privacy Policy describes how Bravas Digital collects, uses, and protects the Personal Information you provide to us through our website, https://www.bravasdigital.com/ (the "Website"). By using the Website, you consent to the collection, use, and disclosure of your Personal Information in accordance with this Privacy Policy.</p>
-              <h3 className='text-base mt-16'>PERSONAL INFORMATION:</h3>
-              <p className='text-sm'>Bravas Digital may collect Personal Information such as your name, email address, phone number, and job title when you interact with the Website, such as when you fill out a contact form, subscribe to our newsletter, or apply for a job. We may also collect information about your use of the Website, such as your IP address, browser type, and operating system.</p>
-              <h3 className='text-base mt-16'>Use of Personal Information:</h3>
-              <p className='text-sm'>Bravas Digital may use your Personal Information to respond to your inquiries, provide you with requested services, send you marketing communications, and improve the Website. We may also use your Personal Information to comply with legal obligations or protect our rights.</p>
-              <h3 className='text-base mt-16'>Disclosure of Personal Information:</h3>
-              <p className='text-sm'>Bravas Digital may share your Personal Information with third-party service providers that assist us in providing the Website, such as website hosting or analytics services. We may also share your Personal Information with law enforcement agencies, regulatory authorities, or other third parties when required by law or to protect our rights or the rights of others.</p>
-              <h3 className='text-base mt-16'>
+        <div className='mx-8 md:mx-16 lg:mx-28 py-20 md:py-24 lg:py-40 bg-white text-black'>
+              <h1 className='text-xl  md:text-2xl lg:text-5xl font-bold'>PRIVACY POLICY</h1>
+              <h3 className='text-xs md:text-base mt-4 md:mt-12 lg:mt-16'>INTRODUCTION:</h3>
+              <p className='text-[10px] md:text-sm'>Bravas Digital is committed to respecting and protecting the privacy of its users. This Privacy Policy describes how Bravas Digital collects, uses, and protects the Personal Information you provide to us through our website, https://www.bravasdigital.com/ (the "Website"). By using the Website, you consent to the collection, use, and disclosure of your Personal Information in accordance with this Privacy Policy.</p>
+        <h3 className='text-xs md:text-base mt-4 md:mt-12 lg:mt-16'>PERSONAL INFORMATION:</h3>
+        <p className='text-[10px] md:text-sm'>Bravas Digital may collect Personal Information such as your name, email address, phone number, and job title when you interact with the Website, such as when you fill out a contact form, subscribe to our newsletter, or apply for a job. We may also collect information about your use of the Website, such as your IP address, browser type, and operating system.</p>
+        <h3 className='text-xs md:text-base mt-4 md:mt-12 lg:mt-16'>Use of Personal Information:</h3>
+        <p className='text-[10px] md:text-sm'>Bravas Digital may use your Personal Information to respond to your inquiries, provide you with requested services, send you marketing communications, and improve the Website. We may also use your Personal Information to comply with legal obligations or protect our rights.</p>
+        <h3 className='text-xs md:text-base mt-4 md:mt-12 lg:mt-16'>Disclosure of Personal Information:</h3>
+        <p className='text-[10px] md:text-sm'>Bravas Digital may share your Personal Information with third-party service providers that assist us in providing the Website, such as website hosting or analytics services. We may also share your Personal Information with law enforcement agencies, regulatory authorities, or other third parties when required by law or to protect our rights or the rights of others.</p>
+        <h3 className='text-xs md:text-base mt-4 md:mt-12 lg:mt-16'>
                   Cookies and Data Collection:</h3>
-              <p className='text-sm'>Bravas Digital uses cookies and similar technologies to track your use of the Website and personalize your experience. Cookies are small files that are stored on your device when you visit the Website. You can control the use of cookies through your browser settings. By using the Website, you consent to the use of cookies as described in this Privacy Policy.</p>
-              <h3 className='text-base mt-16'>Security:</h3>
-              <p className='text-sm'>Bravas Digital takes reasonable measures to protect your Personal Information from unauthorized access, use, or disclosure. However, no data transmission over the internet or storage system can be guaranteed to be 100% secure. Therefore, we cannot guarantee the security of your Personal Information.</p>
-              <h3 className='text-base mt-16'>Retention of Personal Information:</h3>
-              <p className='text-sm'>Bravas Digital will retain your Personal Information for as long as necessary to fulfill the purposes for which it was collected, unless a longer retention period is required by law or necessary to protect our rights.</p>
-              <h3 className='text-base mt-16'>Your Rights:</h3>
-              <p className='text-sm'>You have the right to access, modify, or delete your Personal Information that we hold. You may also have the right to restrict or object to our processing of your Personal Information, or to receive a copy of your Personal Information in a structured, commonly used format. To exercise these rights, please contact us using the information provided below.</p>
+        <p className='text-[10px] md:text-sm'>Bravas Digital uses cookies and similar technologies to track your use of the Website and personalize your experience. Cookies are small files that are stored on your device when you visit the Website. You can control the use of cookies through your browser settings. By using the Website, you consent to the use of cookies as described in this Privacy Policy.</p>
+        <h3 className='text-xs md:text-base mt-4 md:mt-12 lg:mt-16'>Security:</h3>
+        <p className='text-[10px] md:text-sm'>Bravas Digital takes reasonable measures to protect your Personal Information from unauthorized access, use, or disclosure. However, no data transmission over the internet or storage system can be guaranteed to be 100% secure. Therefore, we cannot guarantee the security of your Personal Information.</p>
+        <h3 className='text-xs md:text-base mt-4 md:mt-12 lg:mt-16'>Retention of Personal Information:</h3>
+        <p className='text-[10px] md:text-sm'>Bravas Digital will retain your Personal Information for as long as necessary to fulfill the purposes for which it was collected, unless a longer retention period is required by law or necessary to protect our rights.</p>
+        <h3 className='text-xs md:text-base mt-4 md:mt-12 lg:mt-16'>Your Rights:</h3>
+        <p className='text-[10px] md:text-sm'>You have the right to access, modify, or delete your Personal Information that we hold. You may also have the right to restrict or object to our processing of your Personal Information, or to receive a copy of your Personal Information in a structured, commonly used format. To exercise these rights, please contact us using the information provided below.</p>
 
-              <h1 className=' text-5xl font-bold mt-16'>CONTACT US</h1>
-              <p className='text-sm mt-6'>If you have any questions or concerns about this Privacy Policy or the handling of your Personal Information, please contact us at:</p>
-              <p className='text-sm mt-6'>Email:</p>
+        <h1 className='text-xl mt-4 md:mt-12 lg:mt-16 md:text-2xl lg:text-5xl font-bold'>CONTACT US</h1>
+        <p className='text-[10px] md:text-sm mt-2 md:mt-6'>If you have any questions or concerns about this Privacy Policy or the handling of your Personal Information, please contact us at:</p>
+        <p className='text-[10px] md:text-sm mt-2 md:mt-6'>Email:</p>
               <button className='bg-white border-2 mt-2 border-black px-10 rounded-md '>vaishnav@bravasdigital.com</button>
 
-              <h3 className='text-base mt-8'>Changes to this Privacy Policy:</h3>
-              <p className='text-sm'>Bravas Digital may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on the Website. You are advised to review this Privacy Policy periodically for any changes.</p>
+        <h3 className='text-xs md:text-base mt-4 md:mt-12 lg:mt-16'>Changes to this Privacy Policy:</h3>
+        <p className='text-[10px] md:text-sm'>Bravas Digital may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on the Website. You are advised to review this Privacy Policy periodically for any changes.</p>
         </div>
       
     </div>
